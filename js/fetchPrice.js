83f196bb6566f15874cd1c3af7bf0f3b9d9e3bae
Wale O <dev@screenverve.com>
2021-10-24 05:19:56 -0700

    correct method for updating lastUpdated
    There was one performance.now() that I had left, so it was cutting the
    time intervals between refreshes short. Whoops

--- js/fetchPrice.js
+++ js/fetchPrice.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 const fetchPrice = async (storage) => {
-    storage.setItem('lastUpdated', performance.now())
+    storage.setItem('lastUpdated', Date.now())
     
     return await fetch('https://api.coindesk.com/v1/bpi/currentprice.json').then(res => res.json()).then(({bpi, time}) => ([bpi,time]))
 }
